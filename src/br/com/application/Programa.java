93070e5510ede464f55446b6015d05fc02ecd3e2
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-08 16:41:00 -0300

    Classe de departamento

---
+++ src/br/com/application/Programa.java
@@ -0,0 +1,14 @@
+package br.com.application;
+
+import br.com.model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livro");
+		System.out.println(obj);
+
+	}
+
+}
