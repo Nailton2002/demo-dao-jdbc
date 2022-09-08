1f6ecc09f959d9b35a074a2c3dd968454a352e6a
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-08 16:55:51 -0300

    Classe do vendedor

--- src/br/com/application/Programa.java
+++ src/br/com/application/Programa.java
@@ -1,13 +1,19 @@
 package br.com.application;
 
+import java.util.Date;
+
 import br.com.model.entities.Departamento;
+import br.com.model.entities.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
-		
+
 		Departamento obj = new Departamento(1, "Livro");
+		Vendedor vend = new Vendedor(21, "Nailton", "nailton@email.com", new Date(), 3000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(vend);
 
 	}
 
