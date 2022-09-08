4d5f62e7699783f5ed2f0511478b3f440686bca0
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-08 16:27:44 -0300

    Projeto criado

---
+++ src/br/com/db/DB.java
@@ -0,0 +1,69 @@
+package br.com.db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	private static Connection con = null;
+
+	public static Connection getConnection() {
+		if (con == null) {
+			try {
+				Properties prop = loaProperties();
+				String url = prop.getProperty("dburl");
+				con = DriverManager.getConnection(url, prop);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return con;
+	}
+
+	public static void closeConnection() {
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	private static Properties loaProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+}
